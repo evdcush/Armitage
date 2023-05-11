2cbd6ae689a806557f7b65e0f144da3f70efc153
Evan C <evdcush@protonmail.com>
2023-05-12 05:22:51 +0900

    minor formatting

--- armitage/engine/optim_wrapper_constructors.py
+++ armitage/engine/optim_wrapper_constructors.py
@@ -2,12 +2,14 @@
 constructor.
 
 Optimizer wrapper constructor is used to configure the optimize parameters,
-such as learning rate, momentum in a custom way. You can customize it refer to https://github.com/open-mmlab/mmdetection/blob/3.x/mmdet/engine/optimizers/layer_decay_optimizer_constructor.py
+such as learning rate, momentum in a custom way. You can customize it refer to
+https://github.com/open-mmlab/mmdetection/blob/3.x/\
+mmdet/engine/optimizers/layer_decay_optimizer_constructor.py
 
-The default implementation only does the register process. Users need to rename the
-``CustomOptimWrapperConstructor`` to the real name of the optimizer wrapper
-and implement constructor it.
-"""  # noqa: E501
+The default implementation only does the register process. Users need to
+rename the ``CustomOptimWrapperConstructor`` to the real name of the optimizer
+wrapper and implement constructor it.
+"""
 
 from mmengine.optim import DefaultOptimWrapperConstructor
 
@@ -16,4 +18,4 @@ from armitage.registry import OPTIM_WRAPPER_CONSTRUCTORS
 
 @OPTIM_WRAPPER_CONSTRUCTORS.register_module()
 class CustomOptimWrapperConstructor(DefaultOptimWrapperConstructor):
-    ...
+    pass
