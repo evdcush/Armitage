7a88c40c263d4c508590cf5323b29c2f89cc0088
Evan C <evdcush@protonmail.com>
2023-05-24 21:49:25 +0900

    wip

--- armitage/models/model.py
+++ armitage/models/model.py
@@ -1,7 +1,8 @@
 """This module is used to implement and register the custom model.
 
-Follow the `guide <https://mmengine.readthedocs.io/en/latest/tutorials/model.html>`
-in MMEngine to implement CustomModel
+Follow the
+`guide <https://mmengine.readthedocs.io/en/latest/tutorials/model.html>`
+in MMEngine to implement ``CustomModel``.
 
 The default implementation only does the register process. Users need to rename
 the ``CustomModel`` to the real name of the model and implement it.
--- armitage/models/weight_init.py
+++ armitage/models/weight_init.py
@@ -1,6 +1,8 @@
 """This module is used to implement and register the custom initializer.
 
-MMEngine has implemented varies of initlizers in https://github.com/open-mmlab/mmengine/blob/main/mmengine/model/weight_init.py.
+MMEngine has implemented varies of initlizers in
+https://github.com/open-mmlab/mmengine/blob/main/mmengine/model/weight_init.py
+
 Users can customize the initializer in this file.
 
 The default implementation only does the register process. Users need to rename
